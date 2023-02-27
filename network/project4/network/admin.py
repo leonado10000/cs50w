4870792f099e781f8c3d585bc7839590cdd22f1d
leonado10000 <rjdispair@gmail.com>
2023-02-27 22:46:38 +0530

    webdev prg 1

--- network/project4/network/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from network.models import Post, User, Profile
-# Register your models here.
-
-
-admin.site.register(Post)
-admin.site.register(User)
-admin.site.register(Profile)
